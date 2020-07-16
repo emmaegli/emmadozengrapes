181feb6cb790a8efa39d7806e6ee50a8c397e6e3
Kyle Kaniecki <kanieckikyle@gmail.com>
2020-07-16 17:59:23 -0400

    got tags and categories working for blog posts

--- blog/models.py
+++ blog/models.py
@@ -1,37 +1,97 @@
 from django.db import models
+from django import forms
 
-from wagtail.core.models import Page
+from wagtail.core.models import Page, Orderable
 from wagtail.core.fields import RichTextField
-from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.admin.edit_handlers import FieldPanel, InlinePanel, MultiFieldPanel
+from wagtail.images.edit_handlers import ImageChooserPanel
 from wagtail.search import index
+from wagtail.snippets.models import register_snippet
+from modelcluster.fields import ParentalKey, ParentalManyToManyField
+from modelcluster.contrib.taggit import ClusterTaggableManager
+from taggit.models import TaggedItemBase
+
+
+@register_snippet
+class BlogCategory(models.Model):
+    name = models.CharField(max_length=255)
+    icon = models.ForeignKey(
+        "wagtailimages.Image",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="+",
+    )
+
+    panels = [
+        FieldPanel("name"),
+        ImageChooserPanel("icon"),
+    ]
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "blog categories"
+
+
+class BlogPageTag(TaggedItemBase):
+    content_object = ParentalKey(
+        "BlogPage", related_name="tagged_items", on_delete=models.CASCADE
+    )
 
 
 class BlogIndexPage(Page):
     intro = RichTextField(blank=True)
 
-    content_panels = Page.content_panels + [
-        FieldPanel('intro', classname="full")
-    ]
-
-# Keep the definition of BlogIndexPage, and add:
+    content_panels = Page.content_panels + [FieldPanel("intro", classname="full")]
 
 
 class BlogPage(Page):
     date = models.DateField("Post date")
     intro = models.CharField(max_length=250)
-
-    preview_image = models.ImageField(null=True, blank=True)
-
     body = RichTextField(blank=True)
+    tags = ClusterTaggableManager(through=BlogPageTag, blank=True)
+    categories = ParentalManyToManyField("blog.BlogCategory", blank=True)
+
+    def main_image(self):
+        gallery_item = self.gallery_images.first()
+        if gallery_item:
+            return gallery_item.image
+        else:
+            return None
 
     search_fields = Page.search_fields + [
-        index.SearchField('intro'),
-        index.SearchField('body'),
+        index.SearchField("intro"),
+        index.SearchField("body"),
+        index.SearchField("tags")
     ]
 
     content_panels = Page.content_panels + [
-        FieldPanel('date'),
-        FieldPanel('intro'),
-        FieldPanel('preview_image'),
-        FieldPanel('body', classname="full"),
+        MultiFieldPanel(
+            [
+                FieldPanel("date"),
+                FieldPanel("tags"),
+                FieldPanel("categories", widget=forms.CheckboxSelectMultiple),
+            ],
+            heading="Blog information",
+        ),
+        FieldPanel("intro"),
+        FieldPanel("body", classname="full"),
+        InlinePanel("gallery_images", label="Gallery images"),
+    ]
+
+
+class BlogPageGalleryImage(Orderable):
+    page = ParentalKey(
+        BlogPage, on_delete=models.CASCADE, related_name="gallery_images"
+    )
+    image = models.ForeignKey(
+        "wagtailimages.Image", on_delete=models.CASCADE, related_name="+"
+    )
+    caption = models.CharField(blank=True, max_length=250)
+
+    panels = [
+        ImageChooserPanel("image"),
+        FieldPanel("caption"),
     ]
--- home/models.py
+++ home/models.py
@@ -4,6 +4,8 @@ from wagtail.core.models import Page
 from wagtail.core.fields import RichTextField
 from wagtail.admin.edit_handlers import FieldPanel
 
+from blog.models import BlogPage
+
 
 class HomePage(Page):
     body = RichTextField(blank=True)
@@ -11,3 +13,16 @@ class HomePage(Page):
     content_panels = Page.content_panels + [
         FieldPanel("body", classname="full"),
     ]
+
+    def get_context(self, request, *args, **kwargs):
+        context = super().get_context(request, *args, **kwargs)
+
+        recent_wine_reviews = BlogPage.objects.filter(
+            categories__name="Wine Review"
+        ).reverse()[:3]
+        context["recent_wine_reviews"] = recent_wine_reviews
+        context["recent_blog_posts"] = BlogPage.objects.exclude(
+            id__in=recent_wine_reviews.values_list("id", flat=True)
+        ).order_by("-id")[:3]
+
+        return context
